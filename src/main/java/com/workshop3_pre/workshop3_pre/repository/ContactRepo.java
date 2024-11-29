a0b5ecc229012240fd3e4c9b24dfde86a23a2635
itsjonlol <jonathansuherman@gmail.com>
2024-11-29 19:19:51 +0800

    refactored to generic repo

--- src/main/java/com/workshop3_pre/workshop3_pre/repository/ContactRepo.java
+++ src/main/java/com/workshop3_pre/workshop3_pre/repository/ContactRepo.java
@@ -18,80 +18,7 @@ public class ContactRepo {
     @Autowired
     @Qualifier(Util.template01)
     RedisTemplate<String, String> template;
-    //private RedisTemplate<String,Object> template;
-
-    // public void save(User user) {
-
-        
-    //     template.opsForHash().put("User", user.getId(), user);  // Save contact using its ID as the key
-    // }
-
-
-    // public User getUserById(String id) {
-    //     return (User) template.opsForHash().get("User",id);
-    // }
-
-    // public Boolean checkUserExist(String id) {
-    //     return template.opsForHash().hasKey("User",id);
-    // }
-
-    // public List<Object> getAllUsers() {
-    //     List<Object> objectList = template.opsForHash().values("User");
-    //     System.out.println("Repo list has " + objectList.size());
-    //     return objectList;
-    // }
-
-    // public void save(User user) {
-    //     System.out.println("From repo: user id is " + user.getId());
-    //     String[] userInfo = user.toString().split(",");
-    //     System.out.println(userInfo[0]);
-
-    //     template.opsForHash().put("User", userInfo[0], user.toString());  // Save contact using its ID as the key
-    // }
-
-
-    // public User getUserById(String id) {
-    //     String data = (String) template.opsForHash().get("User",id);
-    //     String[] rawData = data.split(",");
-
-    //     String name = rawData[1];
-    //     String email = rawData[2];
-    //     String phoneNumber = rawData[3];
-    //     String dob = rawData[4];
-    //     LocalDate dateOfBirth = LocalDate.parse(dob);
-        
-    //     User user = new User(id,name,email,phoneNumber,dateOfBirth);
-    //     return user;
-
-
-
-    // }
-
-    // public Boolean checkUserExist(String id) {
-    //     return template.opsForHash().hasKey("User",id);
-    // }
-    // public Boolean deleteUser(String id) {
-    //     Boolean isDeleted = false;
-    //     Long iFound = template.opsForHash().delete("User", id);
-    //     if (iFound>0) {
-    //         isDeleted = true;
-    //     }
-    //     return isDeleted;
-    // }
-
-    // public List<Object> getAllUsers() {
-    //     List<Object> objectList = template.opsForHash().values("User");
-    //     System.out.println("Repo list has " + objectList.size());
-    //     return objectList;
-    // }
-
-    // public void updateUser(User user) {
-    //     // Boolean isUpdated = false;
-    //     String userId = user.getId();
-    //     template.opsForHash().put("User", userId, user.toString());
-
-    // }
-
+    
     // Add or update a single key-value pair in a hash (CREATE)
     public void setHash(String redisKey, String mapKey, String value) {
         template.opsForHash().put(redisKey,mapKey, value);
@@ -172,6 +99,79 @@ public class ContactRepo {
         template.opsForHash().increment(redisKey, mapKey, delta);
     }
 
+    //private RedisTemplate<String,Object> template;
+
+    // public void save(User user) {
+
+        
+    //     template.opsForHash().put("User", user.getId(), user);  // Save contact using its ID as the key
+    // }
+
+
+    // public User getUserById(String id) {
+    //     return (User) template.opsForHash().get("User",id);
+    // }
+
+    // public Boolean checkUserExist(String id) {
+    //     return template.opsForHash().hasKey("User",id);
+    // }
+
+    // public List<Object> getAllUsers() {
+    //     List<Object> objectList = template.opsForHash().values("User");
+    //     System.out.println("Repo list has " + objectList.size());
+    //     return objectList;
+    // }
+
+    // public void save(User user) {
+    //     System.out.println("From repo: user id is " + user.getId());
+    //     String[] userInfo = user.toString().split(",");
+    //     System.out.println(userInfo[0]);
+
+    //     template.opsForHash().put("User", userInfo[0], user.toString());  // Save contact using its ID as the key
+    // }
+
+
+    // public User getUserById(String id) {
+    //     String data = (String) template.opsForHash().get("User",id);
+    //     String[] rawData = data.split(",");
+
+    //     String name = rawData[1];
+    //     String email = rawData[2];
+    //     String phoneNumber = rawData[3];
+    //     String dob = rawData[4];
+    //     LocalDate dateOfBirth = LocalDate.parse(dob);
+        
+    //     User user = new User(id,name,email,phoneNumber,dateOfBirth);
+    //     return user;
+
+
+
+    // }
+
+    // public Boolean checkUserExist(String id) {
+    //     return template.opsForHash().hasKey("User",id);
+    // }
+    // public Boolean deleteUser(String id) {
+    //     Boolean isDeleted = false;
+    //     Long iFound = template.opsForHash().delete("User", id);
+    //     if (iFound>0) {
+    //         isDeleted = true;
+    //     }
+    //     return isDeleted;
+    // }
+
+    // public List<Object> getAllUsers() {
+    //     List<Object> objectList = template.opsForHash().values("User");
+    //     System.out.println("Repo list has " + objectList.size());
+    //     return objectList;
+    // }
+
+    // public void updateUser(User user) {
+    //     // Boolean isUpdated = false;
+    //     String userId = user.getId();
+    //     template.opsForHash().put("User", userId, user.toString());
+
+    // }
   
         
 
--- src/main/java/com/workshop3_pre/workshop3_pre/service/impl/ContactService.java
+++ src/main/java/com/workshop3_pre/workshop3_pre/service/impl/ContactService.java
@@ -19,53 +19,9 @@ public class ContactService {
     @Autowired
     private ContactRepo contactRepo;
     
-    // public void saveUser(User user) {
-    //     contactRepo.save(user);
-    // }
-    // public User getUserById(String id) {
-    //     return contactRepo.getUserById(id);
-    // }
-    // public Boolean checkUserExist(String id) {
-    //     return contactRepo.checkUserExist(id);
-    // }
-    
-    // public Boolean deleteUser(String id) {
-    //     return contactRepo.deleteUser(id);
-    // }
-
-    // public List<User> getAllUsers() {
-    //     List<Object> objects = contactRepo.getAllUsers();
-    //     // System.out.println("Object list has size " + objects.size());
-    //     // List<User> users = objects.stream()
-    //     //                       .filter(obj -> obj instanceof User) // Ensure it's a User instance
-    //     //                       .map(obj -> (User) obj) // Cast to User
-    //     //                       .collect(Collectors.toList());
-    //     // System.out.println("Service list has size: " + users.size());
-
-    //     List<User> users = new ArrayList<>();
-       
-    //     for (Object data : objects) {
-    //         String[] rawData = ((String) data).split(",");
-    //         String id = rawData[0];
-    //         String name = rawData[1];
-    //         String email = rawData[2];
-    //         String phoneNumber = rawData[3];
-    //         String dob = rawData[4];
-    //         LocalDate dateOfBirth = LocalDate.parse(dob);
-            
-    //         User user = new User(id,name,email,phoneNumber,dateOfBirth);
-    //         users.add(user);
-    //     }
-    //     System.out.println("Service list has size: " + users.size());
-        
-    //     return users;
-        
-    // }
-
-
 
     public void saveUser(User user) {
-        contactRepo.setHash("User", user.getId(), user.toString());
+        contactRepo.setHash(Util.redisKey, user.getId(), user.toString());
         
     }
 
@@ -132,4 +88,47 @@ public class ContactService {
         }
         return false;
     }
+
+     // public void saveUser(User user) {
+    //     contactRepo.save(user);
+    // }
+    // public User getUserById(String id) {
+    //     return contactRepo.getUserById(id);
+    // }
+    // public Boolean checkUserExist(String id) {
+    //     return contactRepo.checkUserExist(id);
+    // }
+    
+    // public Boolean deleteUser(String id) {
+    //     return contactRepo.deleteUser(id);
+    // }
+
+    // public List<User> getAllUsers() {
+    //     List<Object> objects = contactRepo.getAllUsers();
+    //     // System.out.println("Object list has size " + objects.size());
+    //     // List<User> users = objects.stream()
+    //     //                       .filter(obj -> obj instanceof User) // Ensure it's a User instance
+    //     //                       .map(obj -> (User) obj) // Cast to User
+    //     //                       .collect(Collectors.toList());
+    //     // System.out.println("Service list has size: " + users.size());
+
+    //     List<User> users = new ArrayList<>();
+       
+    //     for (Object data : objects) {
+    //         String[] rawData = ((String) data).split(",");
+    //         String id = rawData[0];
+    //         String name = rawData[1];
+    //         String email = rawData[2];
+    //         String phoneNumber = rawData[3];
+    //         String dob = rawData[4];
+    //         LocalDate dateOfBirth = LocalDate.parse(dob);
+            
+    //         User user = new User(id,name,email,phoneNumber,dateOfBirth);
+    //         users.add(user);
+    //     }
+    //     System.out.println("Service list has size: " + users.size());
+        
+    //     return users;
+        
+    // }
 }
